3977ac4d04ec0298793eb25e43c55fb4c77df99f
Jacob Gossett <jgossett121@gmail.com>
2016-03-18 11:57:34 -0500

    basic reqs cleaned up

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -2,40 +2,29 @@
 
 var controllers = angular.module('myBlogApp.controllers', []);
 
-controllers.controller('homeController', ['$scope', '$location','Entry', '$routeParams', function($scope, $location, Entry, $routeParams){
+controllers.controller('homeController', ['$scope', '$location', 'Entry', '$routeParams', function($scope, $location, Entry, $routeParams){
     $scope.homeMessage = 'HOMEPAGE';
     $scope.go = function(){
         $location.path('/newpost')
-    }
-    
+    }  
     $scope.allPosts = Entry.query();
-    console.log($scope.allPosts);
-    
 }]);
 
-controllers.controller('newPostController', ['$scope', '$rootScope', 'Entry', '$location', function($scope, $rootScope, Entry, $location){
+controllers.controller('newPostController', ['$scope', 'Entry', '$location', function($scope, Entry, $location){
     $scope.message = 'CREATE NEW POST';
     $scope.newPost = {
         title: '',
         author: '',
         content: ''
     }
-
-    
-    $scope.test = function(){
+    $scope.post = function(){
         $scope.entry = new Entry();
         $scope.entry.data = $scope.newPost;
         Entry.save($scope.entry, function(){
-        console.log($scope.entry.data);
-        console.log($scope.entry.data.title);
         $location.path('/')    
-        })}
-
-    
+    })}    
 }]);
 
 controllers.controller('postViewerController', ['$scope', '$routeParams', 'Entry', function($scope, $routeParams, Entry){
     $scope.post = Entry.get({id:$routeParams.id});
-    console.log($scope.post);
-   
 }]);
